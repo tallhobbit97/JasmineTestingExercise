38cf68b98b69a1e15407739a74566df5c7fc71f2
Juliet Stunkel <rstunkel97@gmail.com>
2023-02-25 16:34:43 -0600

    filled in tests for calculator-test

--- calculator-test.js
+++ calculator-test.js
@@ -1,10 +1,15 @@
 it('should calculate the monthly rate correctly', function () {
-    // ...
+    const values = {
+        amount : 10000,
+        years : 5,
+        rate : 12
+    }
+    expect(calculateMonthlyPayment(values)).toEqual('$222.44');
   });
   
   
   it("should return a result with 2 decimal places", function() {
-    // ..
+    expect(calculateMonthlyPayment(values).toString()).toMatch(/^\d+\.\d\d$/);
   });
   
   /// etc
