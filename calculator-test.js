b434adc7c179987338aaa08e69bf850d0a28e297
Juliet Stunkel <rstunkel97@gmail.com>
2023-02-25 16:43:18 -0600

    fixed the tests so they ran properly

--- calculator-test.js
+++ calculator-test.js
@@ -4,11 +4,16 @@ it('should calculate the monthly rate correctly', function () {
         years : 5,
         rate : 12
     }
-    expect(calculateMonthlyPayment(values)).toEqual('$222.44');
+    expect(calculateMonthlyPayment(values)).toEqual('222.44');
   });
   
   
   it("should return a result with 2 decimal places", function() {
+    const values = {
+        amount : 10000,
+        years : 5,
+        rate : 12
+    }
     expect(calculateMonthlyPayment(values).toString()).toMatch(/^\d+\.\d\d$/);
   });
   
